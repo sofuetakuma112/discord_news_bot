e26bfc49f43a810011c934944ea1dd907eaae883
sofue <kawahagi0620@gmail.com>
2021-02-14 20:26:22 +0900

    [WIP]購読機能追加

--- index.js
+++ index.js
@@ -7,100 +7,107 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const fs = require('fs');
 const csv = require('csv');
 
-const client = new Discord.Client();
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
 
+const client = new Discord.Client();
 const ChannelID = '809472448155746304';
 const milisecondsPerHour = 1000 * 60 * 60;
-
+const milisecondsPerDay = 1000 * 60 * 60 * 24;
 let loadedAllNews;
 
 client.on('ready', () => {
   console.log('ready');
-  const channel = client.channels.cache.find((ch) => ch.id === ChannelID);
-  let lastSentURL;
-  let news;
-  const fetchLatestNews = () => {
-    const req = fetch('https://rss.itmedia.co.jp/rss/2.0/news_bursts.xml');
-    const feedparser = new FeedParser();
-    req.then(
-      function (res) {
-        if (res.status !== 200) {
-          throw new Error('Bad status code');
-        } else {
-          // res.bodyはストリームなのでpipeメソッドで連結できる
-          news = [];
-          res.body.pipe(feedparser);
-        }
-      },
-      function (err) {
-        // リクエストエラーをここに書く
-        console.log('request err', err.message);
-      }
-    );
+  // distribututionNews();
+  // const channel = client.channels.cache.find((ch) => ch.id === ChannelID);
+  // let lastSentURL;
+  // let news;
+  // const fetchLatestNews = () => {
+  //   const req = fetch('https://rss.itmedia.co.jp/rss/2.0/news_bursts.xml');
+  //   const feedparser = new FeedParser();
+  //   req.then(
+  //     function (res) {
+  //       if (res.status !== 200) {
+  //         throw new Error('Bad status code');
+  //       } else {
+  //         // res.bodyはストリームなのでpipeメソッドで連結できる
+  //         news = [];
+  //         res.body.pipe(feedparser);
+  //       }
+  //     },
+  //     function (err) {
+  //       // リクエストエラーをここに書く
+  //       console.log('request err', err.message);
+  //     }
+  //   );
 
-    feedparser.on('error', function (error) {
-      console.log('error event', error.message);
-    });
+  //   feedparser.on('error', function (error) {
+  //     console.log('error event', error.message);
+  //   });
 
-    feedparser.on('readable', function () {
-      // feedparserはストリーム？
-      const stream = this; // thisはfeedpaeserを指し、ストリームである
-      const meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-      let item;
+  //   feedparser.on('readable', function () {
+  //     // feedparserはストリーム？
+  //     const stream = this; // thisはfeedpaeserを指し、ストリームである
+  //     const meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+  //     let item;
 
-      while ((item = stream.read())) {
-        news.push({
-          title: item.title,
-          description: item.description,
-          summary: item.summary,
-          pubDate: item.pubDate,
-          url: item.link,
-        });
-      }
+  //     while ((item = stream.read())) {
+  //       news.push({
+  //         title: item.title,
+  //         description: item.description,
+  //         summary: item.summary,
+  //         pubDate: item.pubDate,
+  //         url: item.link,
+  //       });
+  //     }
 
-      news.sort((a, b) => {
-        if (a.pubDate > b.pubDate) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
+  //     news.sort((a, b) => {
+  //       if (a.pubDate > b.pubDate) {
+  //         return -1;
+  //       } else {
+  //         return 1;
+  //       }
+  //     });
+  //   });
 
-    feedparser.on('end', () => {
-      // デフォルトは50件取ってくる
-      const arrangedNews = [];
-      let slicedNews;
-      if (!lastSentURL) {
-        // 一回目の実行
-        slicedNews = news.slice(0, 1);
-        lastSentURL = slicedNews[0].url;
-      } else {
-        // 2回目以降の配信
-        lastSentNews = news.find((oneNews) => oneNews.url === lastSentURL);
-        const lastSentNewsIndex = news.indexOf(lastSentNews);
-        // インデックスが0 => 前回送信したニュースから新規ニュースが追加されていない
-        if (lastSentNewsIndex !== 0) {
-          slicedNews = news.slice(0, lastSentNewsIndex);
-          lastSentURL = slicedNews[0].url;
-        } else {
-          console.log('新規ニュースが無いです');
-          setTimeout(() => {
-            fetchLatestNews();
-          }, milisecondsPerHour);
-          return;
-        }
-      }
-      for (const oneNews of slicedNews) {
-        arrangedNews.push(`${oneNews.title}\n${oneNews.url}`);
-      }
-      channel.send(arrangedNews.join('\n'));
-      setTimeout(() => {
-        fetchLatestNews();
-      }, milisecondsPerHour);
-    });
-  };
-  fetchLatestNews();
+  //   feedparser.on('end', () => {
+  //     // デフォルトは50件取ってくる
+  //     const arrangedNews = [];
+  //     let slicedNews;
+  //     if (!lastSentURL) {
+  //       // 一回目の実行
+  //       slicedNews = news.slice(0, 1);
+  //       lastSentURL = slicedNews[0].url;
+  //     } else {
+  //       // 2回目以降の配信
+  //       lastSentNews = news.find((oneNews) => oneNews.url === lastSentURL);
+  //       const lastSentNewsIndex = news.indexOf(lastSentNews);
+  //       // インデックスが0 => 前回送信したニュースから新規ニュースが追加されていない
+  //       if (lastSentNewsIndex !== 0) {
+  //         slicedNews = news.slice(0, lastSentNewsIndex);
+  //         lastSentURL = slicedNews[0].url;
+  //       } else {
+  //         console.log('新規ニュースが無いです');
+  //         setTimeout(() => {
+  //           fetchLatestNews();
+  //         }, milisecondsPerHour);
+  //         return;
+  //       }
+  //     }
+  //     for (const oneNews of slicedNews) {
+  //       arrangedNews.push(`${oneNews.title}\n${oneNews.url}`);
+  //     }
+  //     channel.send(arrangedNews.join('\n'));
+  //     setTimeout(() => {
+  //       fetchLatestNews();
+  //     }, milisecondsPerHour);
+  //   });
+  // };
+  // fetchLatestNews();
 });
 
 const csvWriter = createCsvWriter({
@@ -113,14 +120,13 @@ const csvWriter = createCsvWriter({
     { id: 'url', title: 'url' },
   ],
 });
-const allNews = [];
 const fetchAllLatestNews = async () => {
+  const allNews = [];
   const rss_urls = await scraping.fetchRssURLs();
   const promises = [];
   for (const xml of rss_urls) {
     promises.push(
       new Promise(function (resolve, reject) {
-        // ループ完了後に実行したい処理
         const req = fetch(xml.url);
         const feedparser = new FeedParser();
         let news;
@@ -147,7 +153,6 @@ const fetchAllLatestNews = async () => {
         feedparser.on('readable', function () {
           // feedparserはストリーム？
           const stream = this; // thisはfeedpaeserを指し、ストリームである
-          const meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
           let item;
 
           while ((item = stream.read())) {
@@ -173,7 +178,7 @@ const fetchAllLatestNews = async () => {
           console.log('pushed!');
           // 新規追加するニュースに対してループを回す
           for (const oneNews of news) {
-            // 追加済みのallNewsのニュースURLと一致していなければ追加
+            // allNewsに追加済みのニュースURLと一致していなければ追加
             const result = allNews.every((alreadyAddedOneNews) => {
               return alreadyAddedOneNews.url !== oneNews.url;
             });
@@ -188,21 +193,89 @@ const fetchAllLatestNews = async () => {
   }
   Promise.all(promises).then(() => {
     console.log('beforeWrite');
-    csvWriter
-      .writeRecords(allNews)
-      .then(() => console.log('The CSV file was written successfully'));
+    csvWriter.writeRecords(allNews).then(() => {
+      console.log('The CSV file was written successfully');
+      fs.createReadStream(__dirname + '/allNews.csv').pipe(
+        csv.parse({ columns: true }, function (err, data) {
+          loadedAllNews = data;
+        })
+      );
+      setTimeout(async () => {
+        console.log('ニュースを最新情報に更新します');
+        await fetchAllLatestNews();
+        // 購読者へのニュース配信
+      }, 1000 * 60 * 60);
+    });
   });
 };
 // fetchAllLatestNews();
 
+const distribututionNews = async () => {
+  const subscribes = db.collection('subscribes');
+  const snapshot = await subscribes.get();
+  if (snapshot.empty) {
+    console.log('No matching documents.');
+    return;
+  }
+  snapshot.forEach((doc) => {
+    const uidAndKeywords = doc.data();
+    client.users.fetch(uidAndKeywords.uid).then(async (user) => {
+      console.log(user);
+      if (uidAndKeywords.keywords.length !== 0) {
+        filteredNews = loadedAllNews.filter((oneNews) => {
+          const result = [];
+          uidAndKeywords.keywords.forEach((word) => {
+            const reg = new RegExp(word, 'i');
+            result.push(
+              reg.test(oneNews.description) ||
+                reg.test(oneNews.summary) ||
+                reg.test(oneNews.title)
+            );
+          });
+          return result.some((bool) => bool);
+        });
+        if (filteredNews.length !== 0) {
+          const arrangedNews = [];
+          for (const oneNews of filteredNews) {
+            const nextAddNews = `${oneNews.title}\n${oneNews.url}`;
+            arrangedNews.push(nextAddNews);
+          }
+          if (arrangedNews.join('\n').length > 2000) {
+            let chunk = [];
+            for (const newsText of arrangedNews) {
+              if ((chunk.join('\n') + newsText).length < 2000) {
+                chunk.push(newsText);
+              } else {
+                user.send(chunk.join('\n'));
+                chunk = [];
+              }
+            }
+          } else {
+            user.send(arrangedNews.join('\n'));
+          }
+        } else {
+          user.send('一致するニュースがありません');
+        }
+      }
+    });
+  });
+};
+
+// テスト環境ではここからcsvファイルを読み込む
 fs.createReadStream(__dirname + '/allNews.csv').pipe(
   csv.parse({ columns: true }, function (err, data) {
     loadedAllNews = data;
   })
 );
 
+// ここでの呼び出しはdistribututionNews()がreadyイベント発行前に実行されるので
+// 初期化されていないclientに対してメソッドを実行し、エラーとなる
+// distribututionNews();
+
 client.on('message', async (msg) => {
-  const query = /^!q/;
+  const query = /^!q/; // 検索
+  const subscribe = /^!s/; // 購読設定
+  const destroySubscribe = /^!d/; // 購読設定解除
   if (query.test(msg.content)) {
     // 最新のニュース取得
     const channel = client.channels.cache.find((ch) => ch.id === ChannelID);
@@ -211,30 +284,17 @@ client.on('message', async (msg) => {
       .replace(query, '')
       .trim()
       .split(/\s+/);
-    // 広範囲検索
-    // filteredNews = loadedAllNews.filter((oneNews) => {
-    //   const result = [];
-    //   trimedSearchWordsArray.forEach((word) => {
-    //     const reg = new RegExp(word, 'i');
-    //     result.push(
-    //       reg.test(oneNews.description) ||
-    //         reg.test(oneNews.summary) ||
-    //         reg.test(oneNews.title)
-    //     );
-    //   });
-    //   return result.some((bool) => bool);
-    // });
     // 絞り込み検索
     filteredNews = loadedAllNews.filter((oneNews) => {
       const result = [];
-      trimedSearchWordsArray.forEach((word) => {
+      for (const word of trimedSearchWordsArray) {
         const reg = new RegExp(word, 'i');
         result.push(
           reg.test(oneNews.description) ||
             reg.test(oneNews.summary) ||
             reg.test(oneNews.title)
         );
-      });
+      }
       return result.every((bool) => bool);
     });
     if (filteredNews.length !== 0) {
@@ -252,13 +312,60 @@ client.on('message', async (msg) => {
             chunk.push(newsText);
           } else {
             // 次のテキストを追加するとchunkの文字数が2000文字を超える場合
-            channel.send(chunk.join('\n'));
+            await msg.channel.send(chunk.join('\n'));
             chunk = [];
           }
         }
-      } else channel.send(arrangedNews.join('\n'));
+      } else {
+        msg.channel.send(arrangedNews.join('\n'));
+      }
     } else {
-      channel.send('一致するニュースがありません');
+      msg.channel.send('一致するニュースがありません');
+    }
+  } else if (subscribe.test(msg.content)) {
+    const trimedSearchWordsArray = msg.content
+      .replace(subscribe, '')
+      .trim()
+      .split(/\s+/);
+    const snapshot = await db
+      .collection('subscribes')
+      .where('uid', '==', msg.author.id)
+      .get();
+    if (snapshot.empty) {
+      // 一致するものはないから新規追加
+      db.collection('subscribes').add({
+        uid: msg.author.id,
+        keywords: trimedSearchWordsArray,
+      });
+      msg.channel.send(
+        `キーワード「${trimedSearchWordsArray.join(', ')}」で購読します`
+      );
+    } else {
+      snapshot.forEach((doc) => {
+        db.collection('subscribes').doc(doc.id).set({
+          uid: msg.author.id,
+          keywords: trimedSearchWordsArray,
+        });
+        msg.channel.send(
+          `購読するキーワードを「${trimedSearchWordsArray.join(
+            ', '
+          )}」に更新しました`
+        );
+      });
+    }
+  } else if (destroySubscribe.test(msg.content)) {
+    const snapshot = await db
+      .collection('subscribes')
+      .where('uid', '==', msg.author.id)
+      .get();
+    if (snapshot.empty) {
+      // 一致するものはないから新規追加
+      msg.channel.send('...何も起きなかった')
+    } else {
+      snapshot.forEach((doc) => {
+        db.collection('subscribes').doc(doc.id).delete()
+        msg.channel.send('購読設定を解除しました');
+      });
     }
   }
 });
