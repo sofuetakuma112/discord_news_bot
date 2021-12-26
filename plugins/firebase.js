d5c659622426b5a6e3735e6f09f534c7a2ef963f
sofuetakuma <kawahagi0620@gmail.com>
2021-12-27 08:23:19 +0900

    delete read json code

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,5 +1,4 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert({
     type: process.env.FIREBASE_TYPE,
